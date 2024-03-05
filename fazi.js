fea83591a79b17e6b241b8abbb6766cb8ecdf7fa
meerab123 <162276326+meerab123@users.noreply.github.com>
2024-03-05 11:45:13 +0500

    Add files via upload

---
+++ fazi.js
@@ -0,0 +1,19 @@
+// camelcase
+// snake-camelcase
+// pascalcase
+var userName = "noman"; // e.g.camelCase
+var user_name = "owais"; // e.g.snake_case
+var UserName = "chota beam"; // e.g. pascalcase
+var isUserExist = true;
+var is_user_Exist = false;
+var IsUserExist = 1;
+var anyType;
+//console.log(anyType)
+//console.log(userName,user_name , UserName)
+//console.log(isUserExist , is_user_Exist , IsUserExist) 
+// Array => []
+var students = ["noman", "owais", "chota been", "khan bhai"];
+//let firstStudentName = students[1]
+//console.log(firstStudentName)
+//console.log(students[2])
+console.log(students[1], students[2], students[3]);
