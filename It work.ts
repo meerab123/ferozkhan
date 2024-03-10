34ecb5e831cb54a8dc195717666f9978334db568
meerab123 <162276326+meerab123@users.noreply.github.com>
2024-03-11 03:04:11 +0500

    it work.ts

---
+++ It work.ts
@@ -0,0 +1,36 @@
+//let fullName = "feroz khan"
+//console.log (fullName);
+//let fullName : string = "feroz khan"
+//console.log (fullName);
+//let a = 10
+//let b = 15
+//console.log (a , "+",b,"=" a + b)
+//let parsonage : number = 25
+//let finalage : number = parsonage + 5 ;
+//console.log ("parson age is" , finalage)
+//let a = 5
+//let b: boolean = a<10
+//console.log(b)
+//let d = null
+//console.log(d)
+//let g = undefaind
+//console.log(g)
+//camalcase
+//let userName : string = "feroz khan"
+//console.log(userName)
+//snake_case
+//let user_Name : string = "feroz khan"
+//console.log(user_Name)
+//pascalcase
+//let UserName : string = "feroz khan"
+//console.log(UserName)
+let student: string[] = ["fiza","tanzeel","sana","haya","meerab"]
+let firstStudentname = student[4]
+console.log(student[2])
+console.log(firstStudentname)
+console.log(students[1],students[2])
+let fruit: string[] = ["apple" , "banana" , "orange"]
+console.log (fruit[2])
+fruit[1] = "mango"
+console.log (fruit[1])
+
