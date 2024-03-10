cc094302bdb438af0dc292a335d6c1be3557f651
meerab123 <162276326+meerab123@users.noreply.github.com>
2024-03-11 03:05:17 +0500

    Add files via upload

---
+++ quistions18.ts
@@ -0,0 +1,6 @@
+let places: string[] = ["pakistan", "japan", "england", "chaina", "spain"];
+console.log("original order:",places);
+
+console.log("alphabaticale order:");
+
+console.log("original order:")
