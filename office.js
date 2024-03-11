a865ab73c14544b6e7545deb89db4d86b4501a3a
meerab123 <162276326+meerab123@users.noreply.github.com>
2024-03-11 10:54:28 +0500

    Add files via upload

---
+++ office.js
@@ -0,0 +1,35 @@
+//let fullName = "feroz khan"
+//console.log (fullName);
+//let fullName : string = "feroz khan"
+//console.log (fullName);
+//let a = 10
+//let b = 15
+//console.log (a , "+",b,"=" a + b)
+//let parsonage : number = 25
+//let finalage : number = parsonage + 5 ;
+//console.log ("parson age is" , finalage)
+//let a = 5
+//let b: boolean = a<10
+//console.log(b)
+//let d = null
+//console.log(d)
+//let g = undefaind
+//console.log(g)
+//camalcase
+//let userName : string = "feroz khan"
+//console.log(userName)
+//snake_case
+//let user_Name : string = "feroz khan"
+//console.log(user_Name)
+//pascalcase
+//let UserName : string = "feroz khan"
+//console.log(UserName)
+var student = ["fiza", "tanzeel", "sana", "haya", "meerab"];
+var firstStudentnamne = student[4];
+console.log(student[2]);
+console.log("firststudentsname");
+console.log(student[1], student[2]);
+var fruits = ["apple", "orange", "mango"];
+console.log(fruits[2]);
+fruits[1] = "banana";
+console.log(fruits[1]);
