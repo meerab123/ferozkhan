47c601c004a47660180d7547c98c6333e37dbc59
meerab123 <162276326+meerab123@users.noreply.github.com>
2024-03-11 11:03:46 +0500

    Add files via upload

---
+++ feroz.js
@@ -0,0 +1,43 @@
+var username = "amjadkhan";
+console.log(username.toLowerCase());
+console.log(username.toUpperCase());
+console.log(username.charAt(0).toUpperCase().slice(1).toLowerCase());
+var shahid = "\f\t khanbhai \f\t";
+console.log(shahid);
+console.log(shahid.trim());
+var number;
+console.log(18 - 10);
+var favoritenumber = 8;
+console.log(favoritenumber);
+// ferozkhan, 2024-03-08
+// this program prints a parsonal massage.
+var myname = "ferozkhan";
+console.log(myname);
+var string = ["jani", "jan", "jojo"];
+console.log(string);
+var names = ["owais", "nasir", "hamza"];
+//for (let i = 0; i < names.length; i++)
+console.log(names);
+var transports = ["honda motorcycle", "tesla car", "bianchi bicycle"];
+transports.forEach, (1);
+console.log(transports);
+var guests = ["noman", "fiza", "sana"];
+console.log(guests);
+// replace the guest
+var newGast = "naseem shsh";
+console.log(newGast);
+// new invitation
+var guest = "sana";
+console.log(guest);
+var gueast = ["shah", "sami", "alena"];
+console.log("great news! I a bigger dinner table!");
+// adding more guest
+guests.unshift("meerab");
+guests.splice(guests.length / 2, 0, "hayanoor");
+guests.push("lovelife");
+guests.forEach;
+console.log(guests);
+console.log("unfortunately, I can only invite two people for dinner.");
+// new invitation
+var gues = "fiza";
+console.log(guest);
---
+++ feroz.ts
@@ -0,0 +1,59 @@
+let username: string = "amjadkhan"
+console.log(username.toLowerCase())
+console.log(username.toUpperCase())
+console.log(username.charAt(0).toUpperCase().slice(1).toLowerCase())
+
+let shahid: string = "\f\t khanbhai \f\t";
+console.log(shahid);
+console.log(shahid.trim());
+
+let number: "8";
+console.log(18-10);
+
+let favoritenumber: number = 8;
+console.log(favoritenumber);
+
+// ferozkhan, 2024-03-08
+// this program prints a parsonal massage.
+let myname: string = "ferozkhan";
+console.log(myname)
+
+let  string = ["jani","jan","jojo"];
+console.log (string)
+
+let names: string[] = ["owais","nasir","hamza"];
+//for (let i = 0; i < names.length; i++)
+console.log(names)
+
+let transports: string[] = ["honda motorcycle","tesla car","bianchi bicycle"];
+
+transports.forEach,(1)
+console.log(transports);
+
+let guests: string[] = ["noman", "fiza", "sana"];
+console.log(guests)
+
+// replace the guest
+let newGast = "naseem shsh";
+console.log(newGast);
+
+// new invitation
+let guest = "sana";
+
+console.log(guest)
+
+let gueast: string[] =["shah", "sami", "alena"];
+console.log("great news! I a bigger dinner table!");
+
+// adding more guest
+guests.unshift("meerab");
+guests.splice(guests.length / 2, 0, "hayanoor");
+guests.push("lovelife");
+
+guests.forEach
+console.log(guests);
+console.log("unfortunately, I can only invite two people for dinner.");
+
+// new invitation
+let gues = "fiza"
+console.log(guest);
