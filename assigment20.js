4afa5224365e05c12e3656ce72a4076d49035531
meerab123 <162276326+meerab123@users.noreply.github.com>
2024-03-20 02:19:21 +0500

    Add files via upload

---
+++ assigment20.js
@@ -0,0 +1,2 @@
+var students = ["students 1", "student 2"];
+console.log("firstName", "feroz khan", "hassan khan", "cnicno:123456789");
---
+++ assigment20.ts
@@ -0,0 +1,19 @@
+let students = ["students 1","student 2"]
+interface addresstype {
+    streetName: string;
+    city: string;
+    country: string;
+    zipcode: string;
+    cnicno: 123456789,
+    subjact: ["english","programings"]
+    address: "abc road"
+}
+type usertype = {
+    firstName: "feroz khan",
+    lastName: "hassan khan",
+    cnicno: 1232456789
+    isstudent: true,
+    subjact: ["english","programings"]
+    address: "abc road"
+}
+console.log("firstName","feroz khan","hassan khan","cnicno:123456789")
